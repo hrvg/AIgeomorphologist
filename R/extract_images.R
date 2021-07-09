ae26bb9b672d082c8300e4819157473700a73880
hrvg <herve@guillon.xyz>
2021-07-09 15:47:12 -0700

    Added function to extract images

---
+++ R/extract_images.R
@@ -0,0 +1,107 @@
+#' Extract images from Sentinel-2 archice
+#' @param safe_dir `character`, path to the SAFE archives
+#' @param id_tiles `character`, list of S2 tiles
+#' @param labelled_points `sf`, points with a `label`
+#' @param .radius `numeric`, half-dimension of the cropped image in map units
+#' @return a `data.frame` with information about the SiteID and file paths
+#' @export
+#' @import foreach
+#' @import doFuture
+extract_images <- function(safe_dir, id_tiles, labelled_points, .radius = 640){
+  # parse dir
+  safe_dirs <- list.dirs(safe_dir, full.names = TRUE, recursive = FALSE)
+
+  # setting up parallelization
+  doFuture::registerDoFuture()
+  n_cores <- future::availableCores() - 2
+  if(.Platform$OS.type == "unix"){
+    future::plan("multicore", workers = n_cores)
+  } else {
+    future::plan("multisession", workers = n_cores)
+  }
+
+  pb_tiles <- progress::progress_bar$new(format = "[:bar] :current/:total - :percent in :elapsed/:eta \n", total = length(id_tiles), show_after = 0)
+  invisible(pb_tiles$tick(0))
+  res <- foreach(k = seq_along(id_tiles), .combine = rbind) %do% {
+    current_tile <- id_tiles[[k]]
+    current_dirs <- safe_dirs[grepl(current_tile, safe_dirs)]
+    pgr_dirs <- progressr::progressor(along = current_dirs)
+    res <- foreach(j = seq_along(current_dirs), .inorder = FALSE, .combine = rbind) %dopar% {
+      safe_dir <- current_dirs[[j]]
+      dop <- sen2r::safe_getMetadata(safe_dir, info = "fileinfo") %>% 
+        dplyr::pull(.data$sensing_datetime) %>%
+        as.Date()
+      # "B4" "B3" "B2" "B8"
+      .s2_10 <- stars::read_stars(
+        file.path(safe_dir, "MTD_MSIL2A.xml"),
+        proxy = TRUE,
+        quiet = TRUE,
+        sub = 1
+        )
+      # "B5"  "B6"  "B7"  "B8A" "B11" "B12" "AOT" "CLD" "SCL" "SNW" "WVP"
+      .s2_20 <- stars::read_stars(
+        file.path(safe_dir, "MTD_MSIL2A.xml"),
+        proxy = TRUE,
+        quiet = TRUE,
+        sub = 2
+        ) 
+      # "B1"  "B9"  "AOT" "CLD" "SCL" "SNW" "WVP"
+      .s2_60 <- stars::read_stars(
+        file.path(safe_dir, "MTD_MSIL2A.xml"),
+        proxy = TRUE,
+        quiet = TRUE,
+        sub = 3
+        )
+      # red, green, blue
+      .s2_tci <- stars::read_stars(
+        file.path(safe_dir, "MTD_MSIL2A.xml"),
+        proxy = TRUE,
+        quiet = TRUE,
+        sub = 4
+        )
+      points_in_tile <- labelled_points %>% dplyr::filter(id_tile == current_tile)
+      # pgr_pts <- progressr::progressor(nrow(points_in_tile))
+      res <- foreach(i = seq(nrow(points_in_tile)), .combine = rbind) %do% {
+        current_point <- points_in_tile[i, ]
+        current_polygon <- current_point %>%
+        point_to_polygon(radius = .radius, endCapStyle = "SQUARE") %>%
+        sf::st_transform(sf::st_crs(.s2_10))
+        s2_10 <- .s2_10[current_polygon] %>% stars::st_as_stars()
+        s2_20 <- .s2_20[current_polygon] %>% stars::st_as_stars() 
+        s2_20 <- s2_20[,,,seq(6)] %>% stars::st_warp(s2_10)
+        s2_60 <- .s2_60[current_polygon] %>% stars::st_as_stars() 
+        s2_60 <- s2_60[,,,seq(2)] %>% stars::st_warp(s2_10)
+        s2_tci <- .s2_tci[current_polygon] %>% stars::st_as_stars() 
+        all_s2 <- c(s2_10, s2_20, s2_60, s2_tci, along = "band")
+        fpath <- file.path(
+          out_dir,
+          current_point$label,
+          paste0(
+            current_point$SiteID,
+            "_",
+            dop,
+            "_",
+            current_tile,".tif"
+            )
+          )
+        stars::write_stars(all_s2, fpath)
+        df <- data.frame(
+          SiteID = current_point$SiteID, 
+          label = current_point$label, 
+          dop = dop,
+          tile_id = current_tile,
+          fpath = fpath
+          )
+        # pgr_pts(sprintf("i=%g", i))
+        return(df)
+      }
+      pgr_dirs(sprintf("j=%g", j))
+      return(res)
+    }
+    # pgr_tiles(sprintf("k=%g", k))
+    pb_tiles$tick()
+    return(res)
+  } 
+  future::plan("sequential")
+  return(res)
+}
--- Rscript/extract_images.R
+++ Rscript/extract_images.R
@@ -1,47 +1,37 @@
 devtools::load_all()
-library(foreach)
-library(doFuture)
 
+if (as.numeric(R.version$major) >= 4) {
+  progressr::handlers(global = TRUE)
+  progressr::handlers(list(
+    progressr::handler_progress(
+      format   = ":spin :current/:total (:message) [:bar] :percent in :elapsed ETA: :eta",
+      width    = 60,
+      complete = "+"
+      )
+    )
+  )
+}
+
+# init
 safe_dir <- "/media/hguillon/UCD eFlow BigData1/S2_data/SAFE/" # folder to store downloaded SAFE
-out_dir  <- "/media/hguillon/UCD eFlow BigData1/S2_data/sen2r_output/" # output folder
+out_dir  <- "/media/hguillon/datadrive/deep_learning_images/" # output folder
 
-safe_dirs <- list.dirs(safe_dir, full.names = TRUE, recursive = FALSE)
-
-current_tile <- sample(id_tiles, 1)
-print(current_tile)
-
-current_dirs <- safe_dirs[grepl(current_tile, safe_dirs)]
-
-dops <- sen2r::safe_getMetadata(current_dirs, info = "fileinfo") %>%
-  dplyr::pull(.data$sensing_datetime)
-
-fpath <- current_dirs %>% sample(1)
-
-# "B4" "B3" "B2" "B8"
-s2_10 <- stars::read_stars(file.path(fpath, "MTD_MSIL2A.xml"), proxy = TRUE, sub = 1)
-
-# "B5"  "B6"  "B7"  "B8A" "B11" "B12" "AOT" "CLD" "SCL" "SNW" "WVP"
-s2_20 <- stars::read_stars(file.path(fpath, "MTD_MSIL2A.xml"), proxy = TRUE, sub = 2) 
-
-# "B1"  "B9"  "AOT" "CLD" "SCL" "SNW" "WVP"
-s2_60 <- stars::read_stars(file.path(fpath, "MTD_MSIL2A.xml"), proxy = TRUE, sub = 3)
-
-
-s2_tci <- stars::read_stars(file.path(fpath, "MTD_MSIL2A.xml"), proxy = TRUE, sub = 4)
-
+# add id_tiles to labelled_points
 labelled_points <- SSCT_data %>% to_spatial() %>% sf::st_as_sf()
 ind <- sapply(sf::st_intersects(labelled_points, sen2r::s2_tiles()), function(z) if (length(z)==0) NA_integer_ else z[1])
 id_tiles <- sen2r::s2_tiles()[ind, ] %>% dplyr::pull(tile_id)
-labelled_points <- labelled_points %>% dplyr::mutate(id_tile = id_tiles)
+labelled_points <- labelled_points %>% 
+  dplyr::mutate(id_tile = id_tiles) %>%
+  dplyr::mutate(ward = sapply(.data$ward, function(x) paste0("CA-", x))) %>%
+  dplyr::rename(label = .data$ward)
 
-random_point <- labelled_points %>%
-  dplyr::filter(id_tile == current_tile) %>%
-  dplyr::sample_n(1)
+# create outdir
+if(!dir.exists(out_dir)) dir.create(out_dir)
 
-random_polygon <- random_point %>%
-  point_to_polygon(radius = 250, endCapStyle = "SQUARE") %>%
-  sf::st_transform(sf::st_crs(s2_10))
+# create subdirs
+for(x in unique(labelled_points$label)){
+  dname <- file.path(out_dir, x)
+  if(!dir.exists(dname)) dir.create(dname)
+}
 
-x <- s2_20[random_polygon] %>% methods::as("Raster")
-x <- x[[seq(6)]] %>% stars::st_as_stars(proxy = FALSE)
-
+deep_learning_images <- extract_images(safe_dir, id_tiles %>% unique(), labelled_points, .radius = 640)
